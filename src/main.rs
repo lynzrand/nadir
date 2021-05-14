73527dc67b4e80f6779a59887bc2653982285e7e
Rynco Maekawa <lynzrand@outlook.com>
2021-05-14 23:54:13 +0800

    add types

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
+mod eventloop;
+mod view;
+
 use cursive::{
-    theme::{BaseColor::*, Color::*, ColorStyle, Palette, PaletteColor, PaletteColor::*},
+    theme::{BaseColor::*, Color::*, ColorStyle, Palette, PaletteColor::*},
     view::{Margins, Selector},
     views::{self, TextView},
+    View,
 };
+use view::build_list_tag;
 
 #[tokio::main]
 async fn main() {
     let mut siv = cursive::default();
-    let theme = make_theme();
+    let theme = init_theme();
     siv.set_theme(theme);
 
     siv.add_fullscreen_layer(views::Layer::new(views::ResizedView::with_full_screen(
         views::LinearLayout::vertical()
-            .child(views::PaddedView::new(Margins::tb(0, 1), make_stat()))
-            .child(views::TextView::new("Hello World!\nThis is a sample text")),
+            .child(views::PaddedView::new(Margins::tb(0, 1), init_stat()))
+            .child(build_body()),
     )));
     let handle = siv.cb_sink().clone();
 
@@ -24,7 +29,7 @@ async fn main() {
             handle
                 .send(Box::new(|c| {
                     c.call_on(&Selector::Name("time"), |s: &mut TextView| {
-                        s.set_content(make_time());
+                        s.set_content(format_current_time());
                     });
                 }))
                 .unwrap();
@@ -34,11 +39,14 @@ async fn main() {
     tokio::task::block_in_place(|| siv.run());
 }
 
-fn make_stat() -> impl cursive::View {
+fn init_stat() -> impl cursive::View {
     let stat = "NOMINAL";
-    let time = make_time();
+    let time = format_current_time();
 
-    let stat_view = views::TextView::new(stat).style(ColorStyle::front(Light(Green)));
+    let stat_view = views::NamedView::new(
+        "stat",
+        views::TextView::new(stat).style(ColorStyle::front(Light(Green))),
+    );
     let bar = views::TextView::new("|").style(ColorStyle::front(Light(Black)));
     let time_view = views::NamedView::new("time", views::TextView::new(time));
 
@@ -48,15 +56,15 @@ fn make_stat() -> impl cursive::View {
         .child(time_view)
 }
 
-fn make_time() -> String {
+fn format_current_time() -> String {
     let time = chrono::Local::now();
     let time = time.format("%Y-%m-%d %H:%M:%S");
     time.to_string()
 }
 
-fn make_theme() -> cursive::theme::Theme {
+fn init_theme() -> cursive::theme::Theme {
     let mut theme = cursive::theme::Theme::default();
-    let palette = make_palette();
+    let palette = init_palette();
 
     theme.borders = cursive::theme::BorderStyle::None;
     theme.palette = palette;
@@ -64,7 +72,7 @@ fn make_theme() -> cursive::theme::Theme {
     theme
 }
 
-fn make_palette() -> Palette {
+fn init_palette() -> Palette {
     let mut palette = Palette::default();
     palette.extend(vec![
         (Background, TerminalDefault),
@@ -74,4 +82,15 @@ fn make_palette() -> Palette {
         (Secondary, Light(Black)),
     ]);
     palette
+}
+
+fn build_body() -> impl View {
+    let tag = build_list_tag(
+        255,
+        vec!["foo", "bar", "baz"].into_iter(),
+        vec!["qux"].into_iter(),
+        false,
+        true,
+    );
+    views::TextView::new(tag)
 }
---
+++ src/view.rs
@@ -0,0 +1,73 @@
+use std::fmt::{Display, Write};
+
+use cursive::{view::Position, Vec2, View};
+
+pub fn build_list_tag<'a>(
+    count: usize,
+    namespace: impl Iterator<Item = &'a str> + Clone,
+    tags: impl Iterator<Item = &'a str> + Clone,
+    bra: bool,
+    ket: bool,
+) -> String {
+    let mut s = String::new();
+
+    // format count section: "[NNN" or "|" or "<"
+    if count > 1 {
+        s.write_fmt(format_args!("[{}", SimplifyNumber(count)))
+            .unwrap();
+    } else if bra {
+        s.push('[');
+    } else {
+        s.push('|');
+    }
+
+    // format namespace and tags section
+    s.write_fmt(format_args!(
+        "{}",
+        Separated('|', '|', if ket { '>' } else { ']' }, namespace.chain(tags))
+    ))
+    .unwrap();
+
+    s
+}
+
+/// Format a number to at most 4 chars
+struct SimplifyNumber(usize);
+
+impl Display for SimplifyNumber {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.0 < 10000 {
+            write!(f, "{}", self.0)
+        } else if self.0 < 1_000_000 {
+            write!(f, "{}k", self.0 / 1000)
+        } else if self.0 < 1_000_000_000 {
+            write!(f, "{}M", self.0 / 1_000_000)
+        } else if self.0 < 1_000_000_000_000 {
+            write!(f, "{}T", self.0 / 1_000_000_000)
+        } else {
+            write!(f, "{}E", self.0 / 1_000_000_000_000)
+        }
+    }
+}
+
+struct Separated<I>(char, char, char, I);
+
+impl<'a, I, S> Display for Separated<I>
+where
+    I: Iterator<Item = S> + Clone,
+    S: Into<&'a str>,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut it = self.3.clone();
+        let first = it.next();
+        if first.is_none() {
+            return Ok(());
+        }
+        let first = first.unwrap();
+        write!(f, "{}{}", self.0, first.into())?;
+        for rest in it {
+            write!(f, "{}{}", self.1, rest.into())?;
+        }
+        write!(f, "{}", self.2)
+    }
+}
