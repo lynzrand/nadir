e677f63e2954f919782c420065c56708109ec039
Rynco Maekawa <lynzrand@outlook.com>
2021-11-08 12:34:49 +0800

    create project

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod component;
+mod model;
+
+fn main() {
+    let mut rt = tokio::runtime::Builder::new_current_thread()
+        .build()
+        .expect("Failed to build runtime");
+    rt.block_on(main_task())
+}
+
+async fn main_task() {}
+
+async fn notify_task() {}
+
+async fn render_task() {
+    loop {
+        tokio::time::interval(std::time::Duration::from_millis(500))
+            .tick()
+            .await;
+    }
+}
